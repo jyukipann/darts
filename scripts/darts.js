c33577d8342266a32ef5eac6889c02f3c41a352c
JUKI <jyukipann@yahoo.co.jp>
2021-02-21 18:10:51 +0900

    いい感じ

--- scripts/darts.js
+++ scripts/darts.js
@@ -1,15 +1,20 @@
-var canvas = document.createElement("canvas");
+/* var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 document.body.appendChild(canvas);
 var width = document.body.clientWidth;
 var height = window.innerHeight;
 var mouseX = 0, mouseY = 0;
 var mainloop;
+
 resetCanvas();
 hidden();
-
+ */
 var game_mode = "start_menu";
+var game_mode_list = ["01","COUNT-UP","CRICKET"];
+var zero_one_start = 301;
+var round = 8;
 var players = [];
+
 
 function resetCanvas(){
 	canvas.width = width;
@@ -28,7 +33,7 @@ async function resizeCanvas(){
 	canvas.height = height;
 }
 
-function hidden(){
+/* function hidden(){
 	canvas.style.visibility = "hidden";
 }
 
@@ -64,147 +69,223 @@ async function endloop(){
 	cancelAnimationFrame(mainloop);
 	resetCanvas();
 	hidden();
-}
+} */
 
 var all_elements = [];
 var player_inputs = [];
 var players_num = 0;
+var start_menu_div;
 async function start_menu(){
-	var title = document.createElement("h1");
-	all_elements.push(title);
-	title.className = "start_menu";
-	title.innerHTML = 'DARTS';
+	try{
+		start_menu_div.style.display = "inline";
+		console.log("display");
+	}catch(err){
+		console.log("generate");
 
-	var form = document.createElement("form");
-	all_elements.push(form);
-	form.id = "input_player";
-	form.className = "start_menu";
+		start_menu_div = document.createElement("div");
+		start_menu_div.name = "start_menu";
+		start_menu_div.style.display = "inline";
 	
-	var add_player_button = document.createElement("input");
-	all_elements.push(add_player_button);
-	add_player_button.className = "start_menu";
-	add_player_button.type = 'button';
-	add_player_button.value = "add Player";
-	set_last_players();
-	add_player_button.addEventListener("mouseup",() => {add_player_input("")});
+		var title = document.createElement("h1");
+		all_elements.push(title);
+		title.className = "start_menu";
+		title.innerHTML = 'DARTS';
+	
+		var form = document.createElement("form");
+		all_elements.push(form);
+		form.id = "input_player";
+		form.className = "start_menu";
+		
+		var add_player_button = document.createElement("input");
+		all_elements.push(add_player_button);
+		add_player_button.className = "start_menu";
+		add_player_button.type = 'button';
+		add_player_button.value = "add Player";
+		set_last_players();
+		add_player_button.addEventListener("mouseup",() => {add_player_input("")});
+	
+		var selected_label = document.createElement("h2");
+		all_elements.push(selected_label);
+		selected_label.className = "start_menu";
+		selected_label.innerHTML = 'selected : '+game_mode;
+	
+		var radio_button_list = document.createElement("div");
+		all_elements.push(radio_button_list);
+		radio_button_list.className = "start_menu";
 
-	var selected_label = document.createElement("h2");
-	all_elements.push(selected_label);
-	selected_label.className = "start_menu";
-	selected_label.innerHTML = 'selected : '+game_mode;
-
-	var radio_button_list = document.createElement("div");
-	all_elements.push(radio_button_list);
-	radio_button_list.className = "start_menu";
-
-	var game_mode_list = ["01","COUNT-UP","CRICKET"];
-	game_mode_list.forEach(name => make_radio_button(name));
-	let br = document.createElement("br");
-	all_elements.push(br);
-	br.className = "start_menu";
-
-	var players_list = document.createElement("div");
-	all_elements.push(players_list);
-	players_list.className = "start_menu";
-
-	var start_button = document.createElement("input");
-	all_elements.push(start_button);
-	start_button.className = "start_menu";
-	start_button.type = 'button';
-	start_button.value = "start";
-	start_button.addEventListener("mouseup",() => {
-		get_player_name_list();
-		game_start();
-	});
-
-	document.body.appendChild(title);
-	document.body.appendChild(form);
-	form.appendChild(selected_label);
-	form.appendChild(radio_button_list);
-	radio_button_list.appendChild(br);
-	form.appendChild(add_player_button);
-	form.appendChild(players_list);
-	form.appendChild(start_button);
-
-	function set_last_players(){
-		player_inputs = [];
-		players_num = 0;
-		players.forEach(name => {
-			add_player_input(name);
-		});
-	}
-
-	function get_player_name_list(){
-		players = [];
-		player_inputs.forEach(input => {
-			if(input.value === ""){
-				players.push(input.value);
-			}
-		});
-		players_num = players.length;
-	}
-
-	function add_player_input(name){
-		//console.log("add_player_input");
-
-		player_inputs.push(document.createElement("input"));
-		all_elements.push(player_inputs[players_num]);
-		player_inputs[players_num].className = "start_menu";
-		player_inputs[players_num].value = name == ""? "Player " + (players_num+1): name;
-		player_inputs[players_num].type = 'text';
-		player_inputs[players_num].id = players_num;
-		players_list.appendChild(player_inputs[players_num]);
-
-		let delete_button = document.createElement("input");
-		all_elements.push(delete_button);
-		delete_button.type = 'button';
-		delete_button.value = 'delete';
-		delete_button.className = "start_menu";
-		delete_button.id = players_num;
-		delete_button.addEventListener("mouseup",(e)=>{
-			player_inputs[e.target.id].remove();
-			document.getElementById("players_list_br_"+e.target.id).remove();
-			e.target.remove();
-		});
-		players_list.appendChild(delete_button);
-
+		var zero_one_radio_button_list = document.createElement("div");
+		all_elements.push(zero_one_radio_button_list);
+		zero_one_radio_button_list.className = "start_menu";
+		zero_one_radio_button_list.style.display = "none";
+		var zero_one_nums = [301,501,701,901,1101,1301,1501];
+		zero_one_nums.forEach(num => make_zero_one_radio_button(num));
+	
+		game_mode_list.forEach(name => make_radio_button(name));
 		let br = document.createElement("br");
 		all_elements.push(br);
-		br.id = "players_list_br_"+players_num;
 		br.className = "start_menu";
-		players_list.appendChild(br);
-
-		players_num++;
-	}
-
-	function make_radio_button(name){
-		var game_radio_button = document.createElement("input");
-		all_elements.push(game_radio_button);
-		game_radio_button.className = "start_menu";
-		game_radio_button.type = 'radio';
-		game_radio_button.name = 'game_mode';
-		game_radio_button.value = name;
-		game_radio_button.addEventListener("mouseup",(e)=>{
-			game_mode = e.target.value;
-			selected_label.innerHTML = 'selected : ' + game_mode;
+	
+		var players_list = document.createElement("div");
+		all_elements.push(players_list);
+		players_list.className = "start_menu";
+	
+		var start_button = document.createElement("input");
+		all_elements.push(start_button);
+		start_button.className = "start_menu";
+		start_button.type = 'button';
+		start_button.value = "start";
+		start_button.addEventListener("mouseup",() => {
+			get_player_name_list();
+			game_start();
 		});
-		radio_button_list.appendChild(game_radio_button);
-		var game_mode_name_label = document.createElement("span");
-		all_elements.push(game_mode_name_label);
-		game_mode_name_label.className = "start_menu";
-		game_mode_name_label.innerHTML = name;
-		radio_button_list.appendChild(game_mode_name_label);
+	
+		document.body.appendChild(start_menu_div);
+		start_menu_div.appendChild(title);
+		start_menu_div.appendChild(form);
+		form.appendChild(selected_label);
+		form.appendChild(radio_button_list);
+		radio_button_list.appendChild(br);
+		radio_button_list.appendChild(zero_one_radio_button_list);
+		form.appendChild(add_player_button);
+		form.appendChild(players_list);
+		form.appendChild(start_button);
+	
+		function set_last_players(){
+			player_inputs = [];
+			players_num = 0;
+			players.forEach(name => {
+				add_player_input(name);
+			});
+		}
+	
+		function get_player_name_list(){
+			players = [];
+			player_inputs.forEach(input => {
+				if(input.value !== ""){
+					players.push(input.value);
+				}
+			});
+			players_num = players.length;
+		}
+	
+		function add_player_input(name){
+			//console.log("add_player_input");
+	
+			player_inputs.push(document.createElement("input"));
+			all_elements.push(player_inputs[players_num]);
+			player_inputs[players_num].className = "start_menu";
+			player_inputs[players_num].value = name == ""? "Player " + (players_num+1): name;
+			player_inputs[players_num].type = 'text';
+			player_inputs[players_num].id = players_num;
+			players_list.appendChild(player_inputs[players_num]);
+	
+			let delete_button = document.createElement("input");
+			all_elements.push(delete_button);
+			delete_button.type = 'button';
+			delete_button.value = 'delete';
+			delete_button.className = "start_menu";
+			delete_button.id = players_num;
+			delete_button.addEventListener("mouseup",(e)=>{
+				player_inputs[e.target.id].remove();
+				document.getElementById("players_list_br_"+e.target.id).remove();
+				e.target.remove();
+			});
+			players_list.appendChild(delete_button);
+	
+			let br = document.createElement("br");
+			all_elements.push(br);
+			br.id = "players_list_br_"+players_num;
+			br.className = "start_menu";
+			players_list.appendChild(br);
+
+			players_num++;
+		}
+
+		function make_zero_one_radio_button(num){
+			var zero_one_radio_button = document.createElement("input");
+			all_elements.push(zero_one_radio_button);
+			zero_one_radio_button.className = "start_menu";
+			zero_one_radio_button.type = 'radio';
+			zero_one_radio_button.name = 'zero_one_start';
+			zero_one_radio_button.value = num;
+			zero_one_radio_button.addEventListener("mouseup",(e)=>{
+				zero_one_start = Number(e.target.value);
+				selected_label.innerHTML = 'selected : ' + game_mode + " start at " + zero_one_start;
+			});
+			zero_one_radio_button_list.appendChild(zero_one_radio_button);
+			var zero_one_mode_num_label = document.createElement("span");
+			all_elements.push(zero_one_mode_num_label);
+			zero_one_mode_num_label.className = "start_menu";
+			zero_one_mode_num_label.innerHTML = ""+num;
+			zero_one_radio_button_list.appendChild(zero_one_mode_num_label);
+		}
+	
+		function make_radio_button(name){
+			var game_radio_button = document.createElement("input");
+			all_elements.push(game_radio_button);
+			game_radio_button.className = "start_menu";
+			game_radio_button.type = 'radio';
+			game_radio_button.name = 'game_mode';
+			game_radio_button.value = name;
+			game_radio_button.addEventListener("mouseup",(e)=>{
+				game_mode = e.target.value;
+				selected_label.innerHTML = 'selected : ' + game_mode;
+				if(game_mode === "01"){
+					try{
+						zero_one_radio_button_list.style.display = "inline";
+					}catch(err){}
+				}else{
+					zero_one_radio_button_list.style.display = "none";
+				}
+			});
+			radio_button_list.appendChild(game_radio_button);
+			var game_mode_name_label = document.createElement("span");
+			all_elements.push(game_mode_name_label);
+			game_mode_name_label.className = "start_menu";
+			game_mode_name_label.innerHTML = name;
+			radio_button_list.appendChild(game_mode_name_label);
+		}
 	}
 }
 
 function game_start(){
-	console.log("game_start");
+	function debug_log(){
+		console.log("game_start");
+		console.log(players);
+		console.log(game_mode);
+		console.log(round);
+	}
+	var game;
+	switch(game_mode){
+		case "01":
+			if(zero_one_start < 901){
+				round = 8;
+			}else if(zero_one_start < 1301){
+				round = 10;
+			}else{
+				round = 15;
+			}
+			game = zeroOne;
+			break;
+		case "COUNT-UP":
+			round = 8;
+			break;
+		case "CRICKET":
+			round = 20;
+			break;
+		default:
+			game = start_menu;
+	}
+	debug_log();
+	if(players.length > 0){
+		start_menu_div.style.display = "none";
+		game();
+	}
 }
 
-async function zeroOne(startNum,round,players){
-	console.log("zero one : ",startNum);
-	winer = -1;
-
+async function zeroOne(){
+	console.log("zero one : ",zero_one_start);
+	var winer = -1;
 	for(let i = 0; i < round; round--){
 		if(winer > -1){
 			break;
@@ -213,7 +294,6 @@ async function zeroOne(startNum,round,players){
 
 		}
 	}
-
 }
 
 window.addEventListener("load", start_menu);
